8070bb1cc8b204f5b3de4d4ceb5a8c1f7c004fc1
Jorge <jorge.flores20@utrgv.edu>
2023-11-27 16:35:52 -0600

    Added flask

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-# First commit
-print("First commit")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
